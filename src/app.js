834fcc6d3cc844823113e946c1bc8ccd72acaa8c
Pradhityas Waidz Kamal <57702954+waidzk@users.noreply.github.com>
2023-05-22 04:23:44 +0700

    Update app.js, change url responsive styles

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 // Styles
 import "./styles/style.css";
-import "./styles/mobileStyle.css";
+import "./styles/responsiveStyle.css";
 
 //Scripts
 import "./scripts/main";
